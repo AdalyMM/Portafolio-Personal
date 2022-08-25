76b2c0a50c60ce2834248d691672a493cb01fd05
AdalyMM <adaly981121@gmail.com>
2022-08-24 23:09:03 -0500

    Añadiendo responsividad.

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,28 @@
-/*window.onload = () => {
-    gsap.fromTo("#etiqueta-bienvenida", {scale: 0}, {duration: 2, scale: 1.2, ease: "expoScale(0.5, 1.2)"});
-}*/
+window.onunload=function(){
+    window.scrollTo(0,0);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var elemHover = document.getElementsByClassName("contacto");
+flip(elemHover);
+
+
+async function flip(elem) {
+    elemento = elem
+    var logo = document.getElementById(elemento[0].id);
+    logo.classList.add("flip");
+    await sleep(3000);
+    logo.classList.remove("flip");
+    var logo = document.getElementById(elemento[1].id);
+    logo.classList.add("flip");
+    await sleep(3000);
+    logo.classList.remove("flip");
+    var logo = document.getElementById(elemento[2].id);
+    logo.classList.add("flip");
+    await sleep(3000);
+    logo.classList.remove("flip");
+    flip(elemento);
+}
--- routes/router.js
+++ routes/router.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res) {
     res.render('index')
 })
 
+router.get('/proyectos', function(req, res) {
+    res.render('proyectos')   
+})
+
 module.exports = router
